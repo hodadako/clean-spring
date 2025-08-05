c08f1617cfd906302031812b7d8d9356a38abc8f
hodako <kbry12@naver.com>
2025-08-05 14:30:05 +0900

    feat: implemented activate feature

--- splearn/src/main/kotlin/com/splearn/domain/Member.kt
+++ splearn/src/main/kotlin/com/splearn/domain/Member.kt
@@ -5,4 +5,10 @@ class Member(
     var nickname: String,
     var passwordHash: String,
     var status: MemberStatus = MemberStatus.PENDING
-)
+) {
+    fun activate() {
+        check(status == MemberStatus.PENDING) {"PENDING 상태가 아닙니다"}
+
+        status = MemberStatus.ACTIVE
+    }
+}
--- splearn/src/test/kotlin/com/splearn/domain/MemberTest.kt
+++ splearn/src/test/kotlin/com/splearn/domain/MemberTest.kt
@@ -1,13 +1,34 @@
 package com.splearn.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class MemberTest {
     @Test
-    fun `Create Member`() {
+    fun createMember() {
         val member = Member("toby@splearn.app", "Toby", "secret")
 
         assertThat(member.status).isEqualTo(MemberStatus.PENDING)
+    }
+
+    @Test
+    fun activate() {
+        val member = Member("hodako", "hodako", "secret")
+
+        member.activate()
+
+        assertThat(member.status).isEqualTo(MemberStatus.ACTIVE)
+    }
+
+    @Test
+    fun activateFail() {
+        val member = Member("hodako", "hodako", "secret")
+
+        member.activate()
+
+        assertThatThrownBy {
+            member.activate()
+        }.isInstanceOf(IllegalStateException::class.java)
     }
 }
