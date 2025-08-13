aec9c9f3be5049744d455f047c92dc6ba3e02867
hodako <kbry12@naver.com>
2025-08-13 17:15:41 +0900

    test: add missing beans

--- splearn/src/test/kotlin/com/splearn/SplearnApplicationTests.kt
+++ splearn/src/test/kotlin/com/splearn/SplearnApplicationTests.kt
@@ -2,8 +2,10 @@ package com.splearn
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 
-@SpringBootTest
+@SpringBootTest()
+@Import(ApplicationTestSupport::class)
 class SplearnApplicationTests {
 
     @Test
