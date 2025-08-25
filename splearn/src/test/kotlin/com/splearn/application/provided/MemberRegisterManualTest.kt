a3410f0b646e3aafb31799b7df18546e251ee3b6
hodako <kbry12@naver.com>
2025-08-25 23:34:16 +0900

    test: added test with spring boot test

---
+++ splearn/src/test/kotlin/com/splearn/application/provided/MemberRegisterManualTest.kt
@@ -0,0 +1,57 @@
+package com.splearn.application.provided
+
+import com.splearn.application.MemberService
+import com.splearn.application.required.EmailSender
+import com.splearn.application.required.MemberRepository
+import com.splearn.domain.Email
+import com.splearn.domain.Member
+import com.splearn.domain.MemberFixture.createMemberRegisterRequest
+import com.splearn.domain.MemberFixture.createPasswordEncoder
+import com.splearn.domain.MemberStatus
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.Test
+import org.springframework.test.util.ReflectionTestUtils
+
+class MemberRegisterManualTest {
+    @Test
+    fun registerTestStub() {
+        val register = MemberService(MemberRepositoryStub(), EmailSenderStub(), createPasswordEncoder())
+
+        val member = register.register(createMemberRegisterRequest())
+
+        assertAll(
+            { assertThat(member.id).isNotNull() },
+            { assertThat(member.status).isEqualTo(MemberStatus.PENDING) }
+        )
+    }
+
+    @Test
+    fun registerTestMock() {
+        val emailSenderMock = mockk<EmailSender>(relaxed = true)
+
+        val register = MemberService(MemberRepositoryStub(), emailSenderMock, createPasswordEncoder())
+
+        val member = register.register(createMemberRegisterRequest())
+
+        assertAll(
+            { assertThat(member.id).isNotNull() },
+            { assertThat(member.status).isEqualTo(MemberStatus.PENDING) },
+            { verify(exactly = 1) { emailSenderMock.send(member.email, any(), any()) } }
+        )
+    }
+
+    class MemberRepositoryStub : MemberRepository {
+        override fun save(member: Member): Member {
+            ReflectionTestUtils.setField(member, "id", 1L)
+            return member
+        }
+    }
+
+    class EmailSenderStub : EmailSender {
+        override fun send(email: Email, body: String, subject: String) {
+        }
+    }
+}
--- splearn/src/test/kotlin/com/splearn/application/provided/MemberRegisterTest.kt
+++ splearn/src/test/kotlin/com/splearn/application/provided/MemberRegisterTest.kt
@@ -1,57 +1,28 @@
 package com.splearn.application.provided
 
-import com.splearn.application.MemberService
-import com.splearn.application.required.EmailSender
-import com.splearn.application.required.MemberRepository
-import com.splearn.domain.Email
-import com.splearn.domain.Member
-import com.splearn.domain.MemberFixture.createMemberRegisterRequest
-import com.splearn.domain.MemberFixture.createPasswordEncoder
+import com.splearn.ApplicationTestSupport
+import com.splearn.domain.MemberFixture
 import com.splearn.domain.MemberStatus
-import io.mockk.mockk
-import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Test
-import org.springframework.test.util.ReflectionTestUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 
+@SpringBootTest
+@Import(ApplicationTestSupport::class)
 class MemberRegisterTest {
-    @Test
-    fun registerTestStub() {
-        val register = MemberService(MemberRepositoryStub(), EmailSenderStub(), createPasswordEncoder())
+    @Autowired
+    private lateinit var memberRegister: MemberRegister
 
-        val member = register.register(createMemberRegisterRequest())
+    @Test
+    fun register() {
+        val member = memberRegister.register(MemberFixture.createMemberRegisterRequest())
 
         assertAll(
             { assertThat(member.id).isNotNull() },
             { assertThat(member.status).isEqualTo(MemberStatus.PENDING) }
         )
-    }
-
-    @Test
-    fun registerTestMock() {
-        val emailSenderMock = mockk<EmailSender>(relaxed = true)
-
-        val register = MemberService(MemberRepositoryStub(), emailSenderMock, createPasswordEncoder())
-
-        val member = register.register(createMemberRegisterRequest())
-
-        assertAll(
-            { assertThat(member.id).isNotNull() },
-            { assertThat(member.status).isEqualTo(MemberStatus.PENDING) },
-            { verify(exactly = 1) { emailSenderMock.send(member.email, any(), any()) } }
-        )
-    }
-
-    class MemberRepositoryStub : MemberRepository {
-        override fun save(member: Member): Member {
-            ReflectionTestUtils.setField(member, "id", 1L)
-            return member
-        }
-    }
-
-    class EmailSenderStub : EmailSender {
-        override fun send(email: Email, body: String, subject: String) {
-        }
     }
 }
